de9c0ad4b5a95c24400bd0fae9b2141b46f35689
Abiraj UCA <abiraj@unicornconnectedapps.com>
2025-07-29 15:58:15 +0530

    added Nx liraries and lazy loading routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+    pathMatch: 'full',
+  },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('../../libs/products/src/lib/products/products').then(
+        (m) => m.Products
+      ),
+  },
+  {
+    path: 'orders',
+    loadComponent: () =>
+      import('../../libs/orders/src/lib/orders/orders').then(
+        (m) => m.Orders
+      ),
+  },
+];
